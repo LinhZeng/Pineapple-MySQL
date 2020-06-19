0d03ac4895754cf31eca41d5dfaf629c6ee55c37
LinhZ <cenglin@52tt.com>
2020-06-19 18:35:02 +0800

    调整

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 图片展示网站
 瀑布流布局
 Vue + Node+ Express + MySQL
+
+启动服务端
+
+cd server 进入所在目录
+
+npm install 安装依赖
+
+npm start 启动
+
+
+
+启动客户端
+
+cd client 进入所在目录
+
+npm install 安装依赖
+
+npm run dev 启动
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var checkToken = require('../middlewares/check').checkToken;
 var userDao = require('../controllers/user');
+var tokenService = require('../services/token');
 
 // 查询全部用户
 router.get('/list', function(req, res, next) {
@@ -24,12 +25,12 @@ router.post('/login', function(req, res, next) {
 });
 
 // 修改个人信息
-router.post('/editinfo', checkToken,function(req, res, next) {
+router.post('/editinfo', tokenService.verifyRouterToken,function(req, res, next) {
   userDao.updateUserInfo(req,res,next)
 });
 
 // 修改密码
-router.post('/editpsw', checkToken, function(req, res, next) {
+router.post('/editpsw', tokenService.verifyRouterToken, function(req, res, next) {
   userDao.updateUserPSW(req,res,next)
 });
 
